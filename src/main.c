aba26873a13e80cac7574d29bf5b21588ac8ce33
Rowan Decker <Smasher816@gmail.com>
2016-04-03 15:58:36 -0700

    Add Rotary Encoder support
    Based on:
    https://www.circuitsathome.com/mcu/rotary-encoder-interrupt-service-routine-for-avr-micros
    
    This assumes you are using a 2 bit quadrature rotary encoder. Right now
    it is configured to only register detents (both A and B are N/C)
    otherwise the counter will increase/decrease by 4 for each click. If
    using a no-detent encoder than the return logic can be removed.

--- src/main.c
+++ src/main.c
@@ -17,6 +17,27 @@
 
 unsigned long millis;
 
+#define ENC_A PD2
+#define ENC_B PD4
+void enc_init() {
+	DDRD &= ~(_BV(ENC_A) | _BV(ENC_B)); //input
+	PORTD |= _BV(ENC_A) | _BV(ENC_B); //pull-up
+}
+
+int8_t enc_read() {
+	static int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
+	static uint8_t AB = 3;
+
+	uint8_t a = ((PIND & _BV(ENC_A))!=0);
+	uint8_t b = ((PIND & _BV(ENC_B))!=0);
+
+	AB = (AB<<2) | (a<<1) | b;
+	int8_t delta = enc_states[AB & 0x0F];
+
+	//only count valid transitions at a detent
+	return (delta!=0 && a==1 && b==1) ? delta : 0;
+}
+
 int main(void) {
 	MCUSR = 0;
 	wdt_disable();
@@ -34,10 +55,12 @@ int main(void) {
 
 	analog_init();
 	analog_start();
+	enc_init();
 	msgeq7_init();
 	led_init();
 
 	static uint8_t oldBtnState = 0;
+	static uint8_t counter = 100;
 
 	setAdjustment(settings.brightness, &settings.correction, &settings.temperature);
 
@@ -55,6 +78,12 @@ int main(void) {
 					settings_preset(settings.preset);
 				}
 				oldBtnState = btnState;
+			}
+
+			uint8_t x = enc_read();
+			if (x) {
+				counter += x;
+				puti(counter); putchar('\n');
 			}
 
 			*preset.input1.var = (uint8_t)SCALE(analog_sample1(), preset.input1.min, preset.input1.max);
