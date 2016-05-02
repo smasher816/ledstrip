4c19e2be458862308c53f43ccea8bc5acdfe8939
Rowan Decker <Smasher816@gmail.com>
2016-05-02 16:16:42 -0700

    Testing SSD1306 SPI OLED Display
    Not ready to merge into master!

--- src/main.c
+++ src/main.c
@@ -27,10 +27,12 @@ char entries[][16] = {
   "255,255,255",
   "#FFAABB"
 };
-int currentIndex = 2;
+char *title = "MAIN";
+int currentIndex = 1;
 
 unsigned long millis;
 
+#define BTN PD3
 #define ENC_A PD2
 #define ENC_B PD4
 void enc_init() {
@@ -49,7 +51,22 @@ int8_t enc_read() {
 	int8_t delta = enc_states[AB & 0x0F];
 
 	//only count valid transitions at a detent
+#ifdef ENC_DETENT
 	return (delta!=0 && a==1 && b==1) ? delta : 0;
+#else
+	return delta;
+#endif
+}
+
+void showVar(char* title, uint8_t val, uint8_t min, uint8_t max) {
+	oled_move(0,0);//oled_move(0, (128-strlen(title))/2);
+	oled_puts2(title);
+
+	oled_move(4, 0);
+	oled_puti(val, 3);
+
+	oled_move(7,0);
+	oled_pbar((val-min)/(max/127), 128);
 }
 
 int main(void) {
@@ -69,18 +86,23 @@ int main(void) {
 
 	analog_init();
 	analog_start();
-	enc_init();
 	msgeq7_init();
 	led_init();*/
+	enc_init();
 
 	oled_init();
 	oled_clear();
 
-	char s[10] = {' ',' ',' ',' ',' ',' ',' ',' ',' ','\0'};
+	char fps[10] = {0};
+	//char s2[10] = {0};
 	int frames = 0;
 	unsigned long lastUpdate = 0;
-	static uint8_t oldBtnState = 0;
-	static uint8_t counter = 100;
+	static uint8_t oldBtnState = 1;
+	int inMenu = 0;
+	uint8_t val=127;
+
+	DDRD &= ~_BV(BTN); //input
+	PORTD |= _BV(BTN); //pull-up
 
 	//setAdjustment(settings.brightness, &settings.correction, &settings.temperature);
 
@@ -92,49 +114,72 @@ int main(void) {
 			frames++;
 			unsigned long millis = timer_millis();
 			if (millis-lastUpdate > 1000) {
-				itoa(frames, s, 10);
+				itoa(frames, fps, 10);
 				frames = 0;
 				lastUpdate = millis;
 			}
 
-			oled_move(0,0);
-			oled_puts(s);
+			/*oled_move(0,0);
+			oled_puts(title);
+			oled_move(0, 12);
+			oled_puts(fps);
 
-			for (int i=0; i<7; i++) {
-				oled_move(i+1,0);
-				if (i == currentIndex) {
-					oled_putc('>');
-				} else {
-					oled_putc(' ');
+			if (inMenu) {
+				oled_move(1,0);
+				oled_puts("...");
+			} else {
+				oled_move(0,8);
+				oled_puts(s2);
+				for (int i=1; i<7; i++) {
+					oled_move(i,0);
+					if (i == currentIndex) {
+						oled_putc('>');
+					} else {
+						oled_putc(' ');
+					}
+					oled_puts(entries[i-1]);
 				}
-				oled_puts(entries[i]);
-			}
+			}*/
 			////
 
-			/*uint8_t btnState = PINB & _BV(PB0);
+			int8_t x = enc_read();
+			if (x) {
+				/*currentIndex += x;
+				if (currentIndex<1) currentIndex = 1;
+				if (currentIndex>6) currentIndex = 6;
+				itoa(currentIndex, s2, 10);*/
+				val += x;
+				oled_set_contrast(val);
+				showVar("BRIGHTNESS", val, 0, 255);
+			}
+
+			uint8_t btnState = PIND & _BV(BTN);
 			if (btnState != oldBtnState) {
 				if (btnState) {
-					settings.preset++;
+					/*settings.preset++;
 					if (settings.preset == settings.preset_count) {
 						settings.preset = 0;
 					}
-					settings_preset(settings.preset);
+					settings_preset(settings.preset);*/
+					inMenu = !inMenu;
+					if (inMenu) {
+						title = entries[currentIndex-1];
+					} else {
+						title = "MAIN";
+					}
+					oled_clear();
 				}
 				oldBtnState = btnState;
 			}
 
-			uint8_t x = enc_read();
-			if (x) {
-				counter += x;
-				puti(counter); putchar('\n');
-			}
-
-			*preset.input1.var = (uint8_t)SCALE(analog_sample1(), preset.input1.min, preset.input1.max);
+			/*
+			 *preset.input1.var = (uint8_t)SCALE(analog_sample1(), preset.input1.min, preset.input1.max);
 			*preset.input2.var = (uint8_t)SCALE(analog_sample2(), preset.input2.min, preset.input2.max);
 
 			millis = timer_millis();
 			for (int i=0; i<MODE_COUNT; i++) {
 				mode_handlers[preset.modes[i].mode](&preset.modes[i].config);
-			}*/
+			}
+			*/
 	}
 }
--- src/ssd1306.h
+++ src/ssd1306.h
@@ -6,9 +6,6 @@
  */
 
 #pragma once
-#define SS1306_OLED_GEOM_W 128
-#define SS1306_OLED_GEOM_H 64
-
 #define SS1306_OLED_RST PB0 //8
 #define SS1306_OLED_DC  PB1 //9
 #define SS1306_OLED_CS  PB2 //10
@@ -18,27 +15,23 @@
 #define H(port, pin) port |= _BV((pin))
 #define L(port, pin) port &= ~(_BV((pin)))
 
-#define BOX_TL 0x10
-#define BOX_TR 0x0E
-#define BOX_HL 0x40
-#define BOX_VL 0x5D
-#define BOX_BL 0x0D
-#define BOX_BR 0x1D
+void oled_pbar(uint8_t val, uint8_t len);
+void oled_clear_row(uint8_t row);
+void oled_puti(uint8_t val, uint8_t len);
+void oled_putc2(char c);
+void oled_puts2(char *str);
 
 void oled_write(uint8_t dc, uint8_t data);
 void oled_init();
-void oled_display_en(uint8_t state);
-void oled_set_clock(uint8_t val);
+void oled_set_contrast(uint8_t val);
 void oled_set_addr_mode(uint8_t mode);
 void oled_put_tile(uint8_t *tile, uint8_t limit);
-void oled_putc_raw(char c);
 void oled_putc(char c);
-void oled_home();
+void oled_puts(char *str);
 void oled_move_raw(uint8_t row, uint8_t col);
 void oled_move(uint8_t row, uint8_t col);
 void oled_clear();
-void oled_fill(uint8_t row, uint8_t col, uint8_t count, uint8_t max, uint32_t pattern, int8_t pshift);
+/*void oled_fill(uint8_t row, uint8_t col, uint8_t count, uint8_t max, uint32_t pattern, int8_t pshift);
 void oled_putxy(uint8_t left_pxl, uint8_t top_pxl, uint8_t *tile);
-void oled_puts(char *str);
 void oled_bigdigit(uint8_t top, uint8_t left, uint8_t num);
-void oled_box(uint8_t top, uint8_t left, uint8_t width, uint8_t height);
+void oled_box(uint8_t top, uint8_t left, uint8_t width, uint8_t height);*/
